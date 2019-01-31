d3787282de0eab9334bc67a76cb1f8ad5bfdb6b1
Rostyk Broslavsky <broslavsky.rostislaw@gmail.com>
2019-01-31 10:48:49 +0200

    fix conflict

--- dom_practical_task/interactive.js
+++ dom_practical_task/interactive.js
@@ -20,7 +20,7 @@ function setColorForElement(target) {
 }
 
 function showStory(index) {
-    let textChild =  text.querySelectorAll('div');
+    let textChild = text.querySelectorAll('div');
 
     textChild.forEach(el => {
         el.id = "";
@@ -29,37 +29,16 @@ function showStory(index) {
 }
 
 function checkStory({ target }) {
-<<<<<<< HEAD
-    if(target.closest("#navbar > a")) {
+    if (target.closest("#navbar > a")) {
         for (let i = 0; i < menuItem.length; i++) {
             if (menuItem[i] === target) {
                 showStory(i);
             }
-||||||| merged common ancestors
 
-        for (let i = 0; i < menuItem.length; i++) {
-            if (menuItem[i] === target) {//визначаємо на який пункт меню ми натиснули
-                showStory(i);//показуємо її
-            }
-=======
-    for (let i = 0; i < menuItem.length; i++) {
-        if (menuItem[i] === target) {
-            showStory(i);
->>>>>>> 10bd53ff2eb4ec3c29d8beac31ad75a4ac3f0e15
+            setColorForElement(target);
+            hideBurgerMenu(target);
         }
     }
-
-<<<<<<< HEAD
-        setColorForElement(target);
-        hideBurgerMenu(target);
-    }
-||||||| merged common ancestors
-        setColorForElement(target);//додаємо пункту меню відповідний колір
-        hideBurgerMenu(target);//ховаємо navbar якщо екран менше 900 px;
-=======
-    setColorForElement(target);
-    hideBurgerMenu(target);
->>>>>>> 10bd53ff2eb4ec3c29d8beac31ad75a4ac3f0e15
 }
 
 nav.addEventListener("click", checkStory);
